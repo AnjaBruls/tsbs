f96ac59bcf5375c21a887cb8170d66d90366618d
Anja Bruls <anja@transceptor.technology>
2019-01-03 09:08:11 +0100

    siridb

--- scripts/release_siridb.sh
+++ scripts/release_siridb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -r ~/workspace/dbtest/dbpath0/benchmark
-xterm -e ~/workspace/siridb-server/Release/siridb-server -c  ~/workspace/dbtest/siridb0.conf &
+xterm -e ~/workspace/siridb-server_union/Release/siridb-server -c  ~/workspace/dbtest/siridb0.conf &
 
 
 
